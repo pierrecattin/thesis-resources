720ed171c6b5b269df7a9d3882ac76532b4ef688
Pierre Cattin <Pierre Cattin>
2018-06-07 19:11:48 +0200

    fixed bug in test set

--- R/make.ml.sets.R
+++ R/make.ml.sets.R
@@ -26,10 +26,8 @@ make.ml.sets <- function(x, y, training.start, training.end, dev.end, test.end){
   x.full <- x[full.obs, ]
 
   # split into training, dev and test
-  if(training.end<training.start)
-    stop("training.end<training.start")
-  if(dev.end<training.end)
-    stop("dev.end<training.end")
+  stopifnot(training.end>training.start)
+  stopifnot(dev.end>training.end)
 
   training.index <- time(x.full)>=training.start & time(x.full) <= training.end+1
   y.full<- factor(y.full$movement)
@@ -39,18 +37,17 @@ make.ml.sets <- function(x, y, training.start, training.end, dev.end, test.end){
   dev.index <- time(x.full)>training.end+1 & time(x.full)<=dev.end+1
   x.dev <- x.full[dev.index,]
   y.dev <- y.full[dev.index]
-  cat("training set has ", nrow(x.train),"==", length(y.train), " observations \n")
-  cat("dev set has ", nrow(x.dev),"==", length(y.dev), " observations \n")
+  cat("training set has ", nrow(x.train), " observations \n")
+  cat("dev set has ", nrow(x.dev), " observations \n")
 
   if(missing(test.end)){
     return(list(x.train=x.train, x.dev=x.dev, y.train=y.train, y.dev=y.dev))
   } else{
-    if(test.end<dev.end)
-      stop("test.end<dev.end")
-    test.index <- time(x.full)>test.end+1 & time(x.full)<=test.end+1
+    stopifnot(test.end>dev.end)
+    test.index <- time(x.full)>dev.end+1 & time(x.full)<=test.end+1
     x.test <- x.full[test.index,]
     y.test <- y.full[test.index]
-    cat("test set has ", nrow(x.train),"==", length(y.train), " observations \n")
+    cat("test set has ", nrow(x.test), " observations \n")
     return(list(x.train=x.train, x.dev=x.dev, x.test=x.test, y.train=y.train, y.dev=y.dev, y.test=y.test))
   }
 }
