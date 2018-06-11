66f9c5d36d413d42f913a8b351ab88b805035b36
Pierre Cattin <Pierre Cattin>
2018-06-11 15:13:26 +0200

    changing title of plot.freq.vs.accuracy

--- R/plot.freq.vs.accuracy.R
+++ R/plot.freq.vs.accuracy.R
@@ -4,12 +4,12 @@
 #' @param probs : matrix with 2 columns: first is probability of up, second is down
 #' @param conf: numeric indicating if confidence intervall to be plotted (if missing, no intervall is plotted)
 #' @param stock: character (used for title)
-#' @param set: character (used for title)
+#' @param horizon: character or numeric (used for title)
 #'
 #' @return Named numeric containing the results of the best value for min.prop: c(min.prob, dev.accuracy, dev.freq, dev.sr)
 #' @export
 #'
-plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", set="dev"){
+plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", horizon){
   stopifnot(ncol(probs)==2)
   stopifnot(length(y)==nrow(probs))
   stopifnot(all.equal(levels(y), c("-1","1")))
@@ -25,11 +25,11 @@ plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", set="dev"){
   }
   data <- data[!is.na(data$Accuracy), ]
   n.small <- nrow(probs) * data$Frequency * data$Accuracy < 5 | nrow(probs) * data$Frequency * (1-data$Accuracy) < 5
-  data <- data[!n.small, ] # remove
+  data <- data[!n.small, ] # remove datapoints with little observations
 
   plot <- ggplot(data=data, aes(x=Frequency, y=Accuracy)) +
     geom_line(aes(color=min.prob))  +
-    ggtitle(paste0("Accuracy vs frequency of preditions - ", stock,", ", set, " set")) +
+    ggtitle(paste0("Accuracy vs frequency of preditions - ", stock,", ", horizon, "min horizon")) +
     scale_x_continuous(limits = c(0, 1), labels = scales::percent) +
     scale_y_continuous(labels = scales::percent)+
     scale_colour_continuous(name="Min prob.", trans="reverse") + # reverse legend of min.prob
