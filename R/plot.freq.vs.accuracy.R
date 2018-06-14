7622b63b7bb267d1ec4dbe1e42365c38c10b9351
Pierre Cattin <Pierre Cattin>
2018-06-15 00:32:14 +0200

    fixed various issues in plot

--- R/plot.freq.vs.accuracy.R
+++ R/plot.freq.vs.accuracy.R
@@ -1,43 +1,48 @@
 #' Plot frequency vs accuracy of prediction
 #'
-#' @param y  : factor with levels -1, 1
-#' @param probs : matrix with 2 columns: first is probability of up, second is down
-#' @param conf: numeric indicating if confidence intervall to be plotted (if missing, no intervall is plotted)
-#' @param stock: character (used for title)
-#' @param horizon: character or numeric (used for title)
+#' @param y  factor with levels -1, 1
+#' @param probs numeric indicating probability of up
+#' @param conf numeric indicating if confidence intervall to be plotted (if missing, no intervall is plotted)
+#' @param stock character (used for title)
+#' @param horizon character or numeric (used for title)
+#' @param set character (used for title)
+#' @param n number of stocks
 #'
 #' @return ggplot
 #' @export plot.freq.vs.accuracy
 #'
-plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", horizon){
-  stopifnot(ncol(probs)==2)
-  stopifnot(length(y)==nrow(probs))
+plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", horizon, set, n){
+  stopifnot(length(y)==length(probs))
   stopifnot(all.equal(levels(y), c("-1","1")))
 
   min.prob.seq <- seq(0.5, 1, 0.001)
   data <- data.frame(min.prob=min.prob.seq, Accuracy=rep(NA, length(min.prob.seq)), Frequency=rep(NA, length(min.prob.seq)))
   for (min.prob in min.prob.seq){
-    results <- evaluate.predictions(y, probs, min.prob)
+    results <- evaluate.predictions.num(y, probs, min.prob, n)
     if(results[1]=="no predictions") { # if no predictions are made, break the loop
       break
     }
     data[which(min.prob.seq==min.prob), c("Accuracy", "Frequency")] <- results[c("accuracy", "freq")]
   }
   data <- data[!is.na(data$Accuracy), ]
-  n.small <- nrow(probs) * data$Frequency * data$Accuracy < 5 | nrow(probs) * data$Frequency * (1-data$Accuracy) < 5
+  n.small <- length(probs) * data$Frequency  < 20
   data <- data[!n.small, ] # remove datapoints with little observations
 
+
   my.plot <- ggplot(data=data, aes(x=Frequency, y=Accuracy)) +
-    geom_line(aes(color=min.prob))  +
-    ggtitle(paste0("Accuracy vs frequency of preditions - ", stock,", ", horizon, "min horizon")) +
+    geom_line(aes(color=round(min.prob, 1)))  +
+    ggtitle(paste0("accuracy vs frequency of predictions (", stock,", ", set, ", ", horizon, "min,)")) +
     scale_x_continuous(limits = c(0, 1), labels = scales::percent) +
     scale_y_continuous(labels = scales::percent)+
-    scale_colour_continuous(name="Min prob.", trans="reverse") + # reverse legend of min.prob
-    theme_bw()+
+    scale_colour_continuous(name="Min support", trans="reverse") + # reverse legend of min.prob
+    theme_bw(base_size=10) +
+    #  theme_bw(base_size=12,base_family="Times New Roman") +
     theme(legend.position="bottom")
 
   if(!missing(conf)){
-    bounds <- confidence.bound(p=data$Accuracy, n=nrow(probs)*data$Frequency)
+    bounds <- confidence.bound(p=data$Accuracy, n=length(probs)*data$Frequency)
+    bounds[bounds[,1]<0,1] <- 0
+    bounds[bounds[,1]>1,2] <- 1
     data <- cbind(data, bounds)
     my.plot <- my.plot + geom_ribbon(data=data, aes(ymin=lower,ymax=upper, x=Frequency, fill = "darkred"), alpha=0.1) +
       scale_fill_manual( "", labels = paste0(conf*100,"% confidence intervall"), values=c("darkred"="darkred"))
@@ -51,3 +56,4 @@ plot.freq.vs.accuracy <- function (y, probs, conf, stock="stock", horizon){
 # library(ggplot2)
 # stock <- "AAPL"
 # set <- "dev"
+# conf <- 0.95
