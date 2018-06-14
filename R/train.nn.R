106dfbd8c4703877ef7890728835a820b320a8a8
Pierre Cattin <Pierre Cattin>
2018-06-14 16:53:44 +0200

    added tensorboard toggle

--- R/train.nn.R
+++ R/train.nn.R
@@ -10,14 +10,14 @@
 #' @param structure numeric vector indicating number of nodes in each hidden layer
 #' @param l2 l2 regularisation value
 #' @param learning.rate
+#' @param tb.log character inidicating run name for tensorflow. if missing, tensorboard is not ran
 #'
 #' @return fit
 #' @export
 #'
 train.nn <- function(x.train, x.dev, y.train, y.test,
                      epochs, batch.size, activations, structure,
-                     l2, learning.rate){
-
+                     l2, learning.rate, tb.log){
   y.train <- matrix(as.numeric(y.train), ncol=1)-1 # 0 if down, 1 if up
   y.dev <- matrix(as.numeric(y.dev, ncol=1))-1
 
@@ -38,14 +38,25 @@ train.nn <- function(x.train, x.dev, y.train, y.test,
           optimizer = optimizer_adam(lr=learning.rate),
           metrics = c("accuracy", metric.exp.sr)
   )
-  fit <- fit(model,
-             x = x.train,
-             y = y.train,
-             batch_size = batch.size,
-             epochs = epochs,
-             verbose = 1,
-             validation_data =  list(x.dev, y.dev)
-             # callbacks = callback_tensorboard("tf_logs/run_a")
-  )
+
+  if(missing(tb.log)) {
+    fit <- fit(model,
+               x = x.train,
+               y = y.train,
+               batch_size = batch.size,
+               epochs = epochs,
+               verbose = 1,
+               validation_data =  list(x.dev, y.dev))
+  } else {
+    tensorboard(paste0("tb_logs/",tb.log))
+    fit <- fit(model,
+               x = x.train,
+               y = y.train,
+               batch_size = batch.size,
+               epochs = epochs,
+               verbose = 1,
+               validation_data =  list(x.dev, y.dev),
+               callbacks = callback_tensorboard(paste0("tb_logs/",tb.log)))
+  }
   return(fit)
 }
