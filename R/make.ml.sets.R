402fa7667cf8f9f7e343d8e2b8da28d4678d7442
Pierre Cattin <Pierre Cattin>
2018-06-17 12:13:54 +0200

    cleaner split of sets

--- R/make.ml.sets.R
+++ R/make.ml.sets.R
@@ -9,7 +9,7 @@
 #' @param dev.end : date
 #' @param test.end : date. Optional, if missing, no test set is created
 #'
-#' @return list containing x.train, x.dev, (x.test), y.train, y.dev, (y.test)
+#' @return list containing x.train(xts), x.dev(xts), (x.test(xts)), y.train(factor), y.dev(factor), (y.test(factor))
 #' @export
 #'
 make.ml.sets <- function(x, y, training.start, training.end, dev.end, test.end){
@@ -22,21 +22,29 @@ make.ml.sets <- function(x, y, training.start, training.end, dev.end, test.end){
     stop("x and y don't have the same time index")
   }
   full.obs <- (rowSums(is.na(x))==0) & (!is.na(y))
-  y.full <- y[full.obs]
   x.full <- x[full.obs, ]
+
+  y.full <- y[full.obs]
+  stopifnot(all.equal(time(y.full), time(x.full)))
+  y.full <- factor(y.full$movement)
 
   # split into training, dev and test
   stopifnot(training.end>training.start)
   stopifnot(dev.end>training.end)
 
-  training.index <- time(x.full)>=training.start & time(x.full) <= training.end+1
-  y.full<- factor(y.full$movement)
+  days <- date(time(x.full))
+
+  training.index <- days>=training.start & days <= training.end
+  dev.index <- days>training.end & days <= dev.end
+
   x.train <- x.full[training.index,]
   y.train <- y.full[training.index]
-
-  dev.index <- time(x.full)>training.end+1 & time(x.full)<=dev.end+1
   x.dev <- x.full[dev.index,]
   y.dev <- y.full[dev.index]
+  # min(time(x.train)); max(time(x.train))
+  # min(time(x.dev)); max(time(x.dev))
+
+
   cat("training set has ", nrow(x.train), " observations \n")
   cat("dev set has ", nrow(x.dev), " observations \n")
 
@@ -44,10 +52,15 @@ make.ml.sets <- function(x, y, training.start, training.end, dev.end, test.end){
     return(list(x.train=x.train, x.dev=x.dev, y.train=y.train, y.dev=y.dev))
   } else{
     stopifnot(test.end>dev.end)
-    test.index <- time(x.full)>dev.end+1 & time(x.full)<=test.end+1
+    test.index <- days > dev.end & days <= test.end
     x.test <- x.full[test.index,]
     y.test <- y.full[test.index]
     cat("test set has ", nrow(x.test), " observations \n")
+    #min(time(x.test)); max(time(x.test))
     return(list(x.train=x.train, x.dev=x.dev, x.test=x.test, y.train=y.train, y.dev=y.dev, y.test=y.test))
   }
 }
+
+# x <- indicators
+# test.end <- testing.end
+# y <- y[[1]]
