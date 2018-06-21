d5c0ecfd943ab55ece8a11f0fc693629ed475936
Pierre Cattin <Pierre Cattin>
2018-06-21 20:52:22 +0200

    added lagged volume

--- R/compute.lagged.ret.R
+++ R/compute.lagged.ret.R
@@ -4,23 +4,45 @@
 #' @param prices OLHCV prices series; xts
 #' @param max.lag indicates how far back lagged returns should be computed
 #' @param loading logical telling if loading bar should be displayed
+#' @param volume logical indicating if volume should be added
 #'
 #' @return xts with one column per lag. Index is same as index of prices.
 #' @export
 #'
-compute.lagged.ret <- function(prices, max.lag=120, loading=T){
+compute.lagged.ret <- function(prices, max.lag=120, loading=T, volume=T){
 
   all.days <- as.Date(time(prices))
   days <- unique(all.days)
 
   lagged.ret <- matrix(NA, ncol=max.lag, nrow=nrow(prices))
-  colnames(lagged.ret)<- paste0("lag ", 1:max.lag)
+  colnames(lagged.ret)<- paste0("ret.lag.", 1:max.lag)
   lagged.ret <- as.xts(lagged.ret, order.by=time(prices))
-  # d <- days[1]
+  lagged.vol <- lagged.ret
+  colnames(lagged.vol)<- paste0("vol.lag.", 1:max.lag)
+
+  # apply.lagged.ret <- function(n){
+  #   lagged.closes <- closes[1:(length(closes)-n)]
+  #   lagged.closes <- c(rep(NA, n), lagged.closes)
+  #   return(closes/lagged.closes-1)
+  # }
+
+  apply.lagged.ret <- function(n){
+    lagged.closes <- c(NA, closes[1:(length(closes)-1)])
+    lagged.ret <- closes/lagged.closes-1 # one-minute returns
+    lagged.ret <- lagged.ret[1:(length(lagged.ret)-n+1)] # n-1 lags one-minute returns
+    lagged.ret <- c(rep(NA, n-1), lagged.ret)
+    return(lagged.ret)
+  }
+
+  apply.lagged.vol <- function(n){
+    lagged.vol <- volumes[1:(length(volumes)-n)]
+    lagged.vol <- c(rep(NA, n), lagged.vol)
+    return(lagged.vol)
+  }
   if (loading)
     pb.ind <- tkProgressBar(title = "Computing lagged returns", min = 0,
-                          max = length(days), width = 300)
-  d <- days[1]
+                            max = length(days), width = 300)
+  #d <- days[1]
   for (d in days){
     if (loading) {
       day.num <- which(days==d) # only for pb
@@ -28,13 +50,21 @@ compute.lagged.ret <- function(prices, max.lag=120, loading=T){
     }
     closes <- as.numeric(prices[all.days==d,"close"])
 
-    apply.fun <- function(n){
-      return(ROC(closes, n=n, type="discrete"))
+    lagged.ret[all.days==d, ] <- sapply(X=1:max.lag, FUN=apply.lagged.ret)
+
+    if(volume){
+      volumes <- as.numeric(prices[all.days==d,"volume"])
+      lagged.vol[all.days==d, ] <- sapply(X=1:max.lag, FUN=apply.lagged.vol)
     }
-    lagged.ret[all.days==d, ] <- sapply(X=1:max.lag, FUN=apply.fun)
   }
   if (loading)
     close(pb.ind)
 
-  return(lagged.ret)
+  if(volume){
+    features <- cbind(lagged.ret, lagged.vol)
+  } else{
+    features <- lagged.ret
+  }
+  return(features)
 }
+
